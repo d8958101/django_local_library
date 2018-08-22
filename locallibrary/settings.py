126f88ae798b8aa709b2287993f2b515c024542e
Chia-Chi <d8958101@gmail.com>
2018-08-22 11:52:49 +0800

    增加ALLOWED_HOSTS的設定 讓Django可以合法的在header送出類似 fathomless-scrubland-30645.herokuapp.com 的hostname

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -30,7 +30,10 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fathomless-tundra-86897.herokuapp.com','127.0.0.1']
+# For example: 
+# ALLOWED_HOSTS = ['fathomless-scrubland-30645.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
